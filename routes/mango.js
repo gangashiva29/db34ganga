98a284e006da4ec15524045b106f52e4a3b02711
gangashiva29 <shivakumarganga42@gmail.com>
2021-12-01 00:22:55 -0600

    unauthorized

--- routes/mango.js
+++ routes/mango.js
@@ -2,6 +2,16 @@ var express = require('express');
 const mango_controlers = require('../controllers/mango');
 var router = express.Router();
 
+/* A little function to check if we have an authorized user and continue on 
+or 
+// redirect to login. */
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET mangoes */
 router.get('/', mango_controlers.mango_view_all_Page);
 
@@ -9,9 +19,11 @@ router.get('/detail', mango_controlers.mango_view_one_Page);
 
 router.get('/create', mango_controlers.mango_create_Page); 
 
-router.get('/update', mango_controlers.mango_update_Page); 
+router.get('/update', secured, mango_controlers.mango_update_Page); 
 
 router.get('/delete', mango_controlers.mango_delete_Page); 
+
+
 
 
 module.exports = router;
